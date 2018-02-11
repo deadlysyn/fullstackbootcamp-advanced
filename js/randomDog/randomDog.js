7d5becf2a7cb19fe61ec4db243e76c8121c7a1eb
Mike Hoskins <unixdoes@gmail.com>
2018-02-11 16:57:48 -0500

    random dog pics courtesy of XHR

---
+++ js/randomDog/randomDog.js
@@ -0,0 +1,13 @@
+var btn = document.querySelector('#btn')
+var img = document.querySelector('#photo')
+
+btn.addEventListener('click', function() {
+    var XHR = new XMLHttpRequest()
+    XHR.onreadystatechange = function() {
+        if (XHR.readyState == 4 && XHR.status == 200) {
+            img.src = JSON.parse(XHR.responseText).message
+        }
+    }
+    XHR.open('GET', 'https://dog.ceo/api/breeds/image/random')
+    XHR.send();
+})
