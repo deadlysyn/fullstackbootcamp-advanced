ded2f926367da9dcf86ee2b6887afb390955033a
Mike Hoskins <unixdoes@gmail.com>
2018-04-25 00:43:23 -0400

    refactor

--- js/reduce.js
+++ js/reduce.js
@@ -56,13 +56,11 @@ Examples:
 */
 
 function addKeyAndValue(arr, key, value){
-    return arr.reduce(function (accumulator, currentObj) {
-        currentObj[key] = value;
-        accumulator.push(currentObj);
+    return arr.reduce(function (accumulator, currentObj, index) {
+        accumulator[index][key] = value;
         return accumulator;
-    }, []);
+    }, arr);
 }
-
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray.
